0632a123b1d3020955d68c67612fff8626f3358f
Mehran Mazhar <mehran.mazhar@gmail.com>
2024-03-18 20:53:03 +0330

    Update riderequest.rs

--- src/node/riderequest.rs
+++ src/node/riderequest.rs
@@ -1,7 +1,6 @@
 use crate::node::coordinate::Coordinates;
 use serde::{Deserialize, Serialize};
 
-
 #[derive(Serialize, Deserialize)]
 pub struct RideRequest {
     pub pickup_location: Coordinates,
