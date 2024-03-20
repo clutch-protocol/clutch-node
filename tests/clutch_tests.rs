76fa003dbc2f018ac351dfa164d636a0127ff3a7
Mehran Mazhar <mehran.mazhar@gmail.com>
2024-03-20 13:28:33 +0330

    Update clutch_tests.rs

--- tests/clutch_tests.rs
+++ tests/clutch_tests.rs
@@ -3,15 +3,21 @@ use clutch_node::node::{block::Block, *};
 const FROM: &str = "0xMehran"; 
 
 #[test]
-fn test(){
-    
+fn test(){    
+
+    let block1 = ride_request_block();
+
+
+    let blocks:Vec<Block> = vec![block1];
+    let _blockchain = blockchain::Blockchain::new_from_blocks(blocks);    
+}
+
+fn ride_request_block() -> Block {
     let ride_request = ride_request::RideRequest{
         pickup_location : coordinate::Coordinates { latitude: 35.55841414973938, longitude: 51.23861773552397 },//Home
         dropoff_location : coordinate::Coordinates { latitude: 26.649646426996483, longitude: 55.857706441083984 }  //Ghil,hengam iceland
     };
     let ride_request_transcation = transaction::Transaction::ride_request(FROM.to_string(), ride_request);
     let ride_request_block= block::Block::new_block(vec![ride_request_transcation]);
-    
-    let blocks:Vec<Block> = vec![ride_request_block];
-    let _blockchain = blockchain::Blockchain::new_from_blocks(blocks);    
-} 
+    ride_request_block
+}
