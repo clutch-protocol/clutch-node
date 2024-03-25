2c5d71370b99896e30cc6af859a8218076a57483
Mehran Mazhar <mehran.mazhar@gmail.com>
2024-03-25 09:57:18 +0330

    db_base_path

--- src/node/blockchain.rs
+++ src/node/blockchain.rs
@@ -20,7 +20,11 @@ impl Blockchain {
     }
 
     fn rocks_db(name: &str) -> rocksdb::DBWithThreadMode<rocksdb::SingleThreaded> {
-        let db_base_path = env::var("DB_PATH").expect("Failed to get DB_PATH env.");
+        let db_base_path = env::var("DB_PATH").unwrap_or_else(|_| {
+            let current_dir = env::current_dir().expect("Failed to get current directory");
+            current_dir.to_str().unwrap_or(".").to_string()
+        });
+        
         let db_path = format!("{}/{}.db", db_base_path, name);
 
         let mut options = Options::default();
