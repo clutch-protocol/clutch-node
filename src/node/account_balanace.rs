a4df7bea9210d884b56b7fe74639b36be9504949
Mehran Mazhar <mehran.mazhar@gmail.com>
2024-04-11 12:51:59 +0330

    Update account_balanace.rs

--- src/node/account_balanace.rs
+++ src/node/account_balanace.rs
@@ -1,16 +1,16 @@
-use serde::{Deserialize,Serialize};
+use serde::{Deserialize, Serialize};
 
-#[derive(Serialize,Deserialize,Debug)]
-pub struct AccountBalance{
+#[derive(Serialize, Deserialize, Debug)]
+pub struct AccountBalance {
     public_key: String,
-    balance:f64,
+    balance: f64,
 }
 
 impl AccountBalance {
-    pub fn new_account_balance(public_key:String,balance:f64) -> AccountBalance{
-         AccountBalance{
+    pub fn new_account_balance(public_key: String, balance: f64) -> AccountBalance {
+        AccountBalance {
             public_key: public_key,
-            balance :balance
-         }
+            balance: balance,
+        }
     }
 }
