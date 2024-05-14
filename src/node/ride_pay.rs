f77e240972286bb29a9f590bae35893861817d46
Mehran Mazhar <mehran.mazhar@gmail.com>
2024-05-15 00:03:56 +0330

    Update ride_pay.rs

--- src/node/ride_pay.rs
+++ src/node/ride_pay.rs
@@ -2,7 +2,7 @@ use crate::node::{
     account_state::AccountState, ride_acceptance::RideAcceptance, ride_offer::RideOffer,
 };
 
-use super::{database::Database, transaction::Transaction};
+use super::{database::Database, ride_offer, transaction::Transaction};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
@@ -13,35 +13,60 @@ pub struct RidePay {
 
 impl RidePay {
     pub fn verify_state(transaction: &Transaction, db: &Database) -> bool {
-        let ride_pay: Result<RideAcceptance, _> = serde_json::from_str(&transaction.data.arguments);
+        let ride_pay = match serde_json::from_str::<RidePay>(&transaction.data.arguments) {
+            Ok(ride_pay) => ride_pay,
+            Err(_) => {
+                println!("Failed to deserialize transaction data.");
+                return false;
+            }
+        };
 
-        if ride_pay.is_err() {
-            println!("Failed to deserialize transaction data.");
-            return false;
-        }
-
-        let ride_pay = ride_pay.unwrap();
-        let ride_acceptance_tx_hash = &ride_pay.ride_offer_transaction_hash;
-
-        if let Ok(Some(ride_acceptance)) =
-            RideAcceptance::get_ride_acceptance(&ride_acceptance_tx_hash, &db)
+        let ride_acceptance_tx_hash = &ride_pay.ride_acceptance_transaction_hash;
+        let ride_acceptance = match RideAcceptance::get_ride_acceptance(ride_acceptance_tx_hash, db)
         {
-            if let Ok(Some(driver)) =
-                RideOffer::get_from(&ride_acceptance.ride_offer_transaction_hash, &db)
-            {
-                if &driver.to_string() != &transaction.from {
-                    println!("Ride offer 'from' field does not match the transaction 'from' field. Expected: {}, found: {}.", transaction.from, driver);
+            Ok(Some(ride_acceptance)) => ride_acceptance,
+            Ok(None) | Err(_) => {
+                println!("Ride acceptance does not exist or failed to retrieve.");
+                return false;
+            }
+        };
+
+        let ride_offer =
+            match RideOffer::get_ride_offer(&ride_acceptance.ride_offer_transaction_hash, db) {
+                Ok(Some(ride_offer)) => ride_offer,
+                Ok(None) | Err(_) => {
+                    println!(
+                        "Failed to retrieve ride offer '{}'.",
+                        &ride_acceptance.ride_offer_transaction_hash
+                    );
                     return false;
                 }
-            } else {
+            };
+
+        let driver = match RideOffer::get_from(&ride_acceptance.ride_offer_transaction_hash, db) {
+            Ok(Some(driver)) => driver,
+            Ok(None) | Err(_) => {
                 println!(
                     "Failed to retrieve 'from' field for ride offer with transaction hash '{}'.",
                     &ride_acceptance.ride_offer_transaction_hash
                 );
                 return false;
             }
-        } else {
-            println!("Ride acceptance does not exist or failed to retrieve.");
+        };
+
+        if driver.to_string() != transaction.from {
+            println!(
+                "Ride offer 'from' field does not match the transaction 'from' field. Expected: {}, found: {}.",
+                transaction.from, driver
+            );
+            return false;
+        }
+
+        if ride_pay.fare > ride_offer.fare {
+            println!(
+                "The fare in the ride pay ({}) is greater than the fare in the ride offer ({}).",
+                ride_pay.fare, ride_offer.fare
+            );
             return false;
         }
 
@@ -52,23 +77,42 @@ impl RidePay {
         transaction: &Transaction,
         db: &Database,
     ) -> Vec<Option<(Vec<u8>, Vec<u8>)>> {
-        let ride_pay: RidePay = serde_json::from_str(&transaction.data.arguments).unwrap();
+        let ride_pay: RidePay = match serde_json::from_str(&transaction.data.arguments) {
+            Ok(ride_pay) => ride_pay,
+            Err(_) => {
+                eprintln!("Failed to deserialize transaction arguments.");
+                return vec![];
+            }
+        };
+
         let ride_pay_key = Self::construct_ride_pay_key(&transaction.hash);
-        let ride_pay_value = serde_json::to_string(&ride_pay).unwrap().into_bytes();
+        let ride_pay_value = serde_json::to_string(&ride_pay)
+            .expect("Failed to serialize RidePay.")
+            .into_bytes();
 
-        let ride_acceptance_tx_hash = ride_pay.ride_acceptance_transaction_hash;
-        let ride_acceptance = RideAcceptance::get_ride_acceptance(&ride_acceptance_tx_hash, &db);
-        let ride_offer_tx_hash = ride_acceptance
-            .unwrap()
-            .unwrap()
-            .ride_offer_transaction_hash;
-        
-        let ride_fare = ride_pay.fare;
-        let driver = RideOffer::get_from(&ride_offer_tx_hash, &db)
-            .unwrap()
-            .unwrap();
+        let ride_acceptance_tx_hash = &ride_pay.ride_acceptance_transaction_hash;
+        let ride_acceptance = match RideAcceptance::get_ride_acceptance(ride_acceptance_tx_hash, db)
+        {
+            Ok(Some(ride_acceptance)) => ride_acceptance,
+            Ok(None) | Err(_) => {
+                eprintln!("Ride acceptance does not exist or failed to retrieve.");
+                return vec![];
+            }
+        };
 
-        let transfer_value: i64 = ride_fare as i64;
+        let ride_offer_tx_hash = &ride_acceptance.ride_offer_transaction_hash;
+        let driver = match RideOffer::get_from(ride_offer_tx_hash, db) {
+            Ok(Some(driver)) => driver,
+            Ok(None) | Err(_) => {
+                eprintln!(
+                    "Failed to retrieve 'from' field for ride offer with transaction hash '{}'.",
+                    ride_offer_tx_hash
+                );
+                return vec![];
+            }
+        };
+
+        let transfer_value: i64 = ride_pay.fare as i64;
         let (driver_account_state_key, driver_account_state_value) =
             AccountState::update_account_state_key(&driver, transfer_value, db);
 
