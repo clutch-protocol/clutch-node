d5926d925cfea5881e9aa1eb200efacbfc5fceb7
Mehran Mazhar <mehran.mazhar@gmail.com>
2024-06-17 19:03:36 +0330

    Update libp2p.rs

--- src/node/libp2p.rs
+++ src/node/libp2p.rs
@@ -21,7 +21,23 @@ pub struct MyBehaviour {
 pub async fn run() -> Result<(), Box<dyn Error>> {
     setup_tracing()?;
 
-    let mut swarm = libp2p::SwarmBuilder::with_new_identity()
+    let mut swarm = build_swarm()?;
+    let topic = setup_gossipsub_topic(&mut swarm)?;
+
+    listen_for_connections(&mut swarm)?;
+    process_messages(&mut swarm, topic).await
+}
+
+fn setup_tracing() -> Result<(), Box<dyn Error>> {
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .try_init()
+        .expect("setup_tracing error");
+    Ok(())
+}
+
+fn build_swarm() -> Result<Swarm<MyBehaviour>, Box<dyn Error>> {
+    let swarm = libp2p::SwarmBuilder::with_new_identity()
         .with_tokio()
         .with_tcp(
             tcp::Config::default(),
@@ -30,22 +46,19 @@ pub async fn run() -> Result<(), Box<dyn Error>> {
         )?
         .with_quic()
         .with_behaviour(|key| {
-            // To content-address message, we can take the hash of message and use it as an ID.
             let message_id_fn = |message: &gossipsub::Message| {
                 let mut s = DefaultHasher::new();
                 message.data.hash(&mut s);
                 gossipsub::MessageId::from(s.finish().to_string())
             };
 
-            // Set a custom gossipsub configuration
             let gossipsub_config = gossipsub::ConfigBuilder::default()
-                .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
-                .validation_mode(gossipsub::ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message signing)
-                .message_id_fn(message_id_fn) // content-address messages. No two messages of the same content will be propagated.
+                .heartbeat_interval(Duration::from_secs(10))
+                .validation_mode(gossipsub::ValidationMode::Strict)
+                .message_id_fn(message_id_fn)
                 .build()
-                .map_err(|msg| io::Error::new(io::ErrorKind::Other, msg))?; // Temporary hack because `build` does not return a proper `std::error::Error`.
+                .map_err(|msg| io::Error::new(io::ErrorKind::Other, msg))?;
 
-            // build a gossipsub network behaviour
             let gossipsub = gossipsub::Behaviour::new(
                 gossipsub::MessageAuthenticity::Signed(key.clone()),
                 gossipsub_config,
@@ -57,18 +70,7 @@ pub async fn run() -> Result<(), Box<dyn Error>> {
         })?
         .with_swarm_config(|c| c.with_idle_connection_timeout(Duration::from_secs(60)))
         .build();
-
-        let topic = setup_gossipsub_topic(&mut swarm)?;
-
-        listen_for_connections(&mut swarm)?;
-        process_messages(&mut swarm, topic).await
-}
-
-fn setup_tracing() -> Result<(), Box<dyn Error>> {
-    tracing_subscriber::fmt()
-        .with_env_filter(EnvFilter::from_default_env())
-        .try_init().expect("setup_tracing error");
-    Ok(())
+    Ok(swarm)
 }
 
 fn setup_gossipsub_topic(swarm: &mut Swarm<MyBehaviour>) -> Result<IdentTopic, Box<dyn Error>> {
