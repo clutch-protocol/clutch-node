68571b9cddbc79a46eb2c0b8dc58972e1b2d7056
Mehran Mazhar <mehran.mazhar@gmail.com>
2024-06-21 21:39:19 +0330

    f

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use clap::Parser;
-mod node;
 use node::blockchain::Blockchain;
+use tokio::signal;
+use tokio::sync::oneshot;
+
+mod node;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -16,6 +19,29 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config = node::config::AppConfig::from_env(&env)?;
     println!("Loaded configuration from env {:?}: {:?}", &env, &config);
 
-    let _ = Blockchain::new(config.blockchain_name.clone(), config.developer_mode.clone(), config.authorities.clone());
-    node::libp2p::run(config).await
+    let mut blockchain = Blockchain::new(
+        config.blockchain_name.clone(),
+        config.developer_mode.clone(),
+        config.authorities.clone(),
+    );
+
+    let (shutdown_tx, shutdown_rx) = oneshot::channel();
+    tokio::spawn(async move {
+        if let Err(e) = node::libp2p::run(config).await {
+            eprintln!("Error running libp2p: {}", e);
+        }
+        let _ = shutdown_tx.send(());
+    });
+
+    tokio::select! {
+        _ = signal::ctrl_c() => {
+            println!("Received Ctrl+C, shutting down.");
+        }
+        _ = shutdown_rx => {
+            println!("Libp2p task completed, shutting down.");
+        }
+    }
+
+    blockchain.cleanup_if_developer_mode();
+    Ok(())
 }
