ac9b30ea211b47e10168a4150824d2ee1bebcf77
Mehran Mazhar <mehran.mazhar@gmail.com>
2024-08-02 13:39:18 +0330

    fixed

--- src/node/node_services.rs
+++ src/node/node_services.rs
@@ -72,7 +72,14 @@ impl NodeServices {
     ) {
         let listen_addrs: Vec<&str> = config.listen_addrs.iter().map(|s| s.as_str()).collect();
 
-        let mut p2p_server = P2PServer::new(&config.libp2p_topic_name, &listen_addrs).unwrap();
+        let mut p2p_server = match P2PServer::new(&config.libp2p_topic_name, &listen_addrs) {
+            Ok(server) => server,
+            Err(e) => {
+                eprintln!("Failed to create P2PServer: {}", e);
+                return;
+            }
+        };
+        
         tokio::spawn(async move {
             {
                 if let Err(e) = p2p_server.run(Arc::clone(&blockchain), command_rx).await {
